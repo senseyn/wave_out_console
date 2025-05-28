57f4fa2169af0b5a832e884f78d6b6f75f3e91e0
XYQ UwU <117079756+AlexVin0@users.noreply.github.com>
2025-05-28 21:24:02 +0300

    add main.go

---
+++ go.mod
@@ -0,0 +1,4 @@
+module prog2_GO
+
+go 1.24.3
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Create("num.txt")
+	defer file.Close()
+
+	rand.Seed(time.Now().UnixNano())
+	for{
+		rundom := rand.Intn(101)
+		file.WriteString(fmt.Sprintf("%d\n", rundom))
+		count := 40
+		tx := "Записываю."
+		for i:=0; i<count; i++ {
+			count -= 1
+			tx += "."
+			fmt.Println(tx)
+			time.Sleep(50 * time.Millisecond)
+		}
+
+		for i:=60; i>count; i-- {
+			count += 1
+			tx = strings.Replace(tx, ".", "", 1)
+			fmt.Println(tx)
+			time.Sleep(50 * time.Millisecond)
+		}
+	}	
+}
